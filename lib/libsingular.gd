ee6168038dac1ab9fdbe7cbded8b2b53ade6e327
"Max Neunhoeffer ext:(%22) <neunhoef@mcs.st-and.ac.uk>
2011-11-22 10:55:11 +0000

    Add missing types on the GAP level. Correct BUGS file.

--- lib/libsingular.gd
+++ lib/libsingular.gd
@@ -4,26 +4,56 @@ SingularTypes := [];
 
 SingularFamily := NewFamily("SingularFamily");
 DeclareCategory( "IsSingularObj", IsObject );
-DeclareCategory( "IsSingularRing", IsSingularObj );
-DeclareCategory( "IsSingularPoly", IsSingularObj );
 DeclareCategory( "IsSingularBigInt", IsSingularObj );
-DeclareCategory( "IsSingularIntVec", IsSingularObj );
-DeclareCategory( "IsSingularIntMat", IsSingularObj );
 DeclareCategory( "IsSingularIdeal", IsSingularObj );
+DeclareCategory( "IsSingularIntMat", IsSingularObj );
+DeclareCategory( "IsSingularIntVec", IsSingularObj );
+DeclareCategory( "IsSingularLink", IsSingularObj );
+DeclareCategory( "IsSingularList", IsSingularObj );
+DeclareCategory( "IsSingularMap", IsSingularObj );
+DeclareCategory( "IsSingularMatrix", IsSingularObj );
+DeclareCategory( "IsSingularModule", IsSingularObj );
+DeclareCategory( "IsSingularNumber", IsSingularObj );
+DeclareCategory( "IsSingularPoly", IsSingularObj );
+DeclareCategory( "IsSingularQRing", IsSingularObj );
+DeclareCategory( "IsSingularResolution", IsSingularObj );
+DeclareCategory( "IsSingularRing", IsSingularObj );
+DeclareCategory( "IsSingularString", IsSingularObj );
+DeclareCategory( "IsSingularVector", IsSingularObj );
 DeclareCategory( "IsSingularProxy", IsPositionalObjectRep and IsSingularObj );
 
-SingularTypes[SINGULAR_TYPENRS.SINGTYPE_RING] 
-   := NewType(SingularFamily,IsSingularRing);
-SingularTypes[SINGULAR_TYPENRS.SINGTYPE_POLY]
-   := NewType(SingularFamily,IsSingularPoly);
 SingularTypes[SINGULAR_TYPENRS.SINGTYPE_BIGINT]
    := NewType(SingularFamily,IsSingularBigInt);
-SingularTypes[SINGULAR_TYPENRS.SINGTYPE_INTVEC]
-   := NewType(SingularFamily,IsSingularIntVec);
-SingularTypes[SINGULAR_TYPENRS.SINGTYPE_INTMAT]
-   := NewType(SingularFamily,IsSingularIntMat);
 SingularTypes[SINGULAR_TYPENRS.SINGTYPE_IDEAL]
    := NewType(SingularFamily,IsSingularIdeal);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_INTMAT]
+   := NewType(SingularFamily,IsSingularIntMat);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_INTVEC]
+   := NewType(SingularFamily,IsSingularIntVec);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_LINK]
+   := NewType(SingularFamily,IsSingularLink);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_LIST]
+   := NewType(SingularFamily,IsSingularList);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_MAP]
+   := NewType(SingularFamily,IsSingularMap);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_MATRIX]
+   := NewType(SingularFamily,IsSingularMatrix);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_MODULE]
+   := NewType(SingularFamily,IsSingularModule);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_NUMBER]
+   := NewType(SingularFamily,IsSingularNumber);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_POLY]
+   := NewType(SingularFamily,IsSingularPoly);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_QRING] 
+   := NewType(SingularFamily,IsSingularQRing);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_RESOLUTION] 
+   := NewType(SingularFamily,IsSingularResolution);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_RING] 
+   := NewType(SingularFamily,IsSingularRing);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_STRING] 
+   := NewType(SingularFamily,IsSingularString);
+SingularTypes[SINGULAR_TYPENRS.SINGTYPE_VECTOR] 
+   := NewType(SingularFamily,IsSingularVector);
 
 BindGlobal("SingularProxiesType", NewType( SingularFamily, IsSingularProxy ));
 
