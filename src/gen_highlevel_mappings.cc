c8d2ec13962279afc95913f33f752b184cf435b9
"Max Neunhoeffer ext:(%22) <neunhoef@mcs.st-and.ac.uk>
2011-11-25 21:28:33 +0000

    New program to dump tables as GAP code.

---
+++ src/gen_highlevel_mappings.cc
@@ -0,0 +1,12 @@
+#include <gmp.h>
+#include <string>
+#include <iostream>
+#include <libsingular.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello\n";
+    return 0;
+}
