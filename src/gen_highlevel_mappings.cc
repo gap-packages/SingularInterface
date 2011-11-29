2ebd32d5a21ce9bcaaf85495c1e4d4ae3f3bcfd3
"Max Neunhoeffer ext:(%22) <neunhoef@mcs.st-and.ac.uk>
2011-11-29 00:29:47 +0000

    Try to access tables in singular.

--- src/gen_highlevel_mappings.cc
+++ src/gen_highlevel_mappings.cc
@@ -8,5 +8,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     cout << "Hello\n";
+    siInit("/home/neunhoef/4.0/pkg/libsingular/Singular-3-1-3/Singular/libsingular.so");
+    for (int tok = 0;tok < MAX_TOK;tok++)
+        cout << "Command:" << Tok2Cmdname(tok) << ":token:" 
+             << tok << "\n";
     return 0;
 }
