11d95d58194deab7472e3e887cfb6cbd35402cd8
Max Horn <max@quendi.de>
2014-01-07 14:24:35 +0100

    Forgot to commit two files

--- src/libsing.h
+++ src/libsing.h
@@ -171,6 +171,10 @@ inline void SET_ATTRIB_SINGOBJ( Obj obj, void *a )
 
 Obj NEW_SINGOBJ(UInt type, void *cxx);
 Obj NEW_SINGOBJ_RING(UInt type, void *cxx, Obj ring);
+Obj NEW_SINGOBJ_ZERO_ONE(UInt type, void *cxx, Obj zero, Obj one);
+
+void StartPrintCapture();
+void EndPrintCapture();
 
 
 #if 0
