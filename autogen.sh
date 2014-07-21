0fee88c56c89c1f42696fd7298cb353300d97ec8
Max Horn <max@quendi.de>
2014-07-22 00:30:43 +0200

    autogen.sh: error out if autoconf/automake missing

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
+
+if ! command -v autoreconf >/dev/null 2>&1 ; then
+    echo "Please install GNU autoconf and GNU automake."
+    exit 1
+fi
 
 autoreconf -vif `dirname "$0"`
